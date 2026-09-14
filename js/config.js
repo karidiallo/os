5e04f972ef7f47828d08f67cc51f094ea821c6fb
karidiallo <ktdiallo.pro@gmail.com>
2026-09-14 17:06:12 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-export const SUPABASE_URL = "https://ucvmmbyjxotvpsupvbxp.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_x029j3ULUkvd2mBUchrntg_fKRF0sIW";
-
-// Po pierwszym udanym logowaniu możesz ustawić false,
-// żeby aplikacja nie tworzyła nowych kont przez Magic Link.
-export const ALLOW_SIGNUP = true;
