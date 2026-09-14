63c500a47cf887be8d7f423a926d98d84bef49ee
karidiallo <ktdiallo.pro@gmail.com>
2026-09-14 17:35:09 +0200

    Delete supabase-patch-v1_1.sql

--- supabase-patch-v1_1.sql
+++
@@ -1,5 +0,0 @@
--- Personal OS v1.1 patch
--- Run once in Supabase SQL Editor.
--- Allows task duration to be unknown (NULL).
-alter table public.tasks
-  alter column estimate_minutes drop not null;
